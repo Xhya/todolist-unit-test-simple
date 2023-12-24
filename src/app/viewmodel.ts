3a0cfaac2b1c59c1df4c2cf11e4a1af212f65a18
alexiasouvane <alexia.souvane@gmail.com>
2023-12-24 18:30:52 +0100

    viewmodel

--- src/app/viewmodel.ts
+++ src/app/viewmodel.ts
@@ -1,8 +1,10 @@
-import { DeepSignal } from "deepsignal/react";
+import { DeepSignal, deepSignal } from "deepsignal/react";
+import S from "s-js";
+import { GlobalState } from "./globalState";
+import { globalStateObservable } from "./globalState.observable";
 import Repository from "./repository";
 import { RepositoryInterface } from "./repositoryInterface";
 import { TodolistType } from "./types";
-import { globalStateObservable } from "./globalState.observable";
 
 export type State = {
   list: TodolistType;
@@ -16,22 +18,32 @@ export default class ViewModel {
 
   constructor(repository: RepositoryInterface = new Repository()) {
     this.repository = repository;
+    S.on(globalStateObservable, () => {
+      this.state.list = globalStateObservable().list;
+    });
   }
 
-  state: DeepSignal<State> = globalStateObservable;
+  state: DeepSignal<State> = deepSignal({
+    list: globalStateObservable().list,
+    input: "",
+    isLoading: false,
+    get isAddButtonDisabled(): boolean {
+      return !!!this.input;
+    },
+  });
 
   async fetchData() {
     const list = await this.repository.getList();
-    this.state.list = list;
+    globalStateObservable({ ...GlobalState, list });
   }
 
   async addItem(text: string) {
     if (text) {
-      this.state.isLoading = true;
+      this.state.isLoading = true
       const list = await this.repository.addItem(text);
-      this.state.list = list;
+      globalStateObservable({ ...GlobalState, list });
       this.state.input = "";
-      this.state.isLoading = false;
+      this.state.$isLoading = false
     }
   }
 
