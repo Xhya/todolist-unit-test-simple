7416d1f9639a11615967725167d50705a0647eb1
alexiasouvane <alexia.souvane@gmail.com>
2024-01-02 16:34:27 +0100

    organisation code

--- src/app/Todolist.component.tsx
+++ src/app/Todolist.component.tsx
@@ -2,67 +2,47 @@ import { useEffect, useState } from "react";
 import ViewModel from "./viewmodel";
 import { useSignalEffect } from "@preact/signals-react";
 import "./todolist.css";
-
-const VM = new ViewModel();
+import { TodolistType } from "./types";
 
 function Todolist() {
-  //   const [input, setInput] = useState("");
-  //   const [isAddButtonDisabled, setIsAddButtonDisabled] = useState(true);
-  //   const [isLoading, setIsLoading] = useState(false);
-  // const [list, setList] = useState<TodolistType>([])
+  const [input, setInput] = useState("");
+  const [isAddButtonDisabled, setIsAddButtonDisabled] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [list, setList] = useState<TodolistType>([]);
 
-  const state = VM.state
-  const input = state.input;
-  const list = state.list;
-  const isLoading = state.isLoading;
-  const isAddButtonDisabled = state.isAddButtonDisabled;
-
-  // On va fetcher la données pour afficher l'état initial
-  //   useEffect(() => {
-  //     async function fetchData() {
-  //       const response = await fetch("./data.json");
-  //       const json = await response.json();
-  //       setList(json);
-  //     }
-  //     fetchData();
-  //   }, []);
-
-  // on écoute sur 'input' pour savoir si le bouton Ajouter est disabled ou non
-  //   useEffect(() => {
-  //     if (input) {
-  //       setIsAddButtonDisabled(false);
-  //     } else {
-  //       setIsAddButtonDisabled(true);
-  //     }
-  //   }, [input]);
-
-  useSignalEffect(() => {
-    const fetchData = async () => {
-      await VM.fetchData();
-    };
+  // Afficher l'état initial
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("./data.json");
+      const json = await response.json();
+      setList(json);
+    }
     fetchData();
-  });
+  }, []);
 
-  const onClickValidate = () => {
-    VM.addItem(input);
+  // Rajouter un item
+  // Afficher un état de chargement
+  const onClickValidate = async () => {
+    if (input) {
+      setIsLoading(true);
+      const response = await fetch("./data_add.json");
+      const json = await response.json();
+      await sleep(2000);
+      setList(json);
+      setIsLoading(false);
+      setInput("");
+    }
   };
 
-  const onUpdateInput = (input: string) => {
-    VM.onUpdateInput(input);
+  // Ne pas autoriser l'ajout d'un item si aucun mot écrit
+  const onUpdateInput = (newInput: string) => {
+    if (input) {
+      setIsAddButtonDisabled(false);
+    } else {
+      setIsAddButtonDisabled(true);
+    }
+    setInput(newInput);
   };
-
-  // lors de l'ajout d'un item on met la todo en était de Loading en attendant la réponse serveur
-  //   const onClickValidate = async () => {
-  //     if (input) {
-  //       setIsLoading(true);
-  //       const response = await fetch("./data_add.json");
-  //       const json = await response.json();
-  //       await sleep(2000);
-  //       setList(json);
-  //       setIsLoading(false);
-  //       setInput("");
-  //     }
-  //   };
 
   return (
     <div className="todolist">
@@ -78,7 +58,7 @@ function Todolist() {
         </button>
       </div>
       <div className="list">
-        {list.map((item, index) => (
+        {(list as TodolistType).map((item) => (
           <div className="item" key={item.text + item.id}>
             <span>{item.text}</span>
           </div>
